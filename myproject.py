1da71e298b77413f7f5aaa73a7dc2de9b6383918
kobayashi <takumikobayashi0704@gmail.com>
2023-10-05 14:14:59 +0000

    add upload form

--- myproject.py
+++ myproject.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+from fractions import Fraction
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 import os
-from PIL import Image, ExifTags
+import piexif
 
 app = Flask(__name__, static_folder="./static/")
 IMAGE_DIR = './static/images'
+
+class UploadForm(FlaskForm):
+    photo = FileField('Choose a photo', validators=[FileRequired()])
 
 def get_images():
     images = []
@@ -15,6 +25,23 @@ def get_images():
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_image():
+    form = UploadForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        # アップロードされた画像を保存
+        photo = form.photo.data
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], photo.filename)
+        photo.save(filename)
+
+        # 画像のファイル名をデータベースなどに保存することもできます
+        print(exif_info(filename))
+        piexif.remove(filename)
+
+        return redirect(url_for('upload_image'))
+
+    return render_template('upload.html', form=form)
 
 @app.route("/gallery")
 def garally():
